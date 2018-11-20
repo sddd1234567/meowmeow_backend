a0ccf643bfb8771cc216dfd2bd97f1817651ab64
Chen <sddd1234567@gmail.com>
2018-11-21 01:33:42 +0800

    綠界api

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var request = require('request');
 var crypto = require('crypto');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
+var ecpay_payment = require('ECPAY_Payment_node_js');
 //增加靜態檔案的路徑
 app.use(express.static('public'))
 
@@ -22,6 +23,10 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://meowmeow-31087.firebaseio.com"
 });
+
+
+let inv_params = {
+};
 
 
 
@@ -44,30 +49,61 @@ app.get('/donate', function (req, res) {
     var tradeNo = CreateTradeNo();
     var orderDate = GetDateStr();
     var orderInfo = {
-        ChoosePayment: "CVS",
         ChooseSubPayment: "IBON",
         ClientBackURL: '/',
-        EncryptType: "1",
         ItemName: "中途之家捐款",
-        MerchantID: "2000132",
         MerchantTradeDate: orderDate,
         MerchantTradeNo: tradeNo,
-        PaymentInfoURL: "http://140.127.220.111/finishCreateOrder",
         PaymentType: "aio",
         ReturnURL: "http://140.127.220.111/finishPay",
-        StoreID: "",
         TotalAmount: 50,
         TradeDesc: "捐款給中途之家"
     }
-    orderInfo.CheckMacValue = CheckMacValue(orderInfo);
+    let base_param = {
+        ChooseSubPayment: "IBON",
+        MerchantTradeNo: tradeNo, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
+        MerchantTradeDate: orderDate, //ex: 2017/02/13 15:45:30
+        TotalAmount: '50',
+        TradeDesc: '捐款給中途之家',
+        ItemName: '中途之家捐款',
+        ReturnURL: 'http://140.127.220.111/finishPay',
+        // ChooseSubPayment: '',
+        // OrderResultURL: 'http://192.168.0.1/payment_result',
+        // NeedExtraPaidInfo: '1',
+        // ClientBackURL: 'https://www.google.com',
+        // ItemURL: 'http://item.test.tw',
+        // Remark: '交易備註',
+        // StoreID: '',
+        // CustomField1: '',
+        // CustomField2: '',
+        // CustomField3: '',
+        // CustomField4: ''
+    };
+    let cvs_params = {
+        StoreExpireDate: '7',
+        Desc_1: '超商螢幕描述A',
+        Desc_2: '超商螢幕描述B',
+        Desc_3: '超商螢幕描述C',
+        Desc_4: '超商螢幕描述D',
+        PaymentInfoURL: 'http://140.127.220.111/finishCreateOrder'
+    };
+    let client_redirect = '';
+    let create = new ecpay_payment();
+    let htm = create.payment_client.aio_check_out_cvs(cvs_info = cvs_params, parameters = base_param, invoice = inv_params, client_redirect_url = client_redirect);
+    res.send(htm);
+    console.log(htm);
+    // orderInfo.CheckMacValue = CheckMacValue(orderInfo);
     // console.log(JSON.stringify(orderInfo));
     // res.send(orderInfo);
     // res.send(crypto.createHash('sha256').update('hashkey%3d5294y06jbispm5x9%26choosepayment%3dcvs%26choosesubpayment%3dcvs%26clientbackurl%3dhttps%3a%2f%2fdevelopers.opay.tw%2faiomock%2fmerchantclientbackurl%26encrypttype%3d1%26itemname%3d%e4%b8%ad%e9%80%94%e4%b9%8b%e5%ae%b6%e6%8d%90%e6%ac%be%26merchantid%3d2000132%26merchanttradedate%3d2018%2f11%2f2+21%3a15%3a5%26merchanttradeno%3dtfirslpmt0rba03hirab%26paymenttype%3daio%26returnurl%3dhttp%3a%2f%2f140.127.220.111%2ffinishpay%26storeid%3d%26totalamount%3d50%26tradedesc%3d%e6%8d%90%e6%ac%be%e7%b5%a6%e4%b8%ad%e9%80%94%e4%b9%8b%e5%ae%b6%26hashiv%3dv77hokgq4kwxnnis').digest());
     // CreateNewOrder(orderInfo);
-    request.post({ url: 'https://payment-stage.ecpay.tw/Cashier/AioCheckOut/V5', form: orderInfo }, function (err, httpResponse, body) {
-        console.log('finish');
-            res.send(body);
-        })
+    
+    // request.post({ url: 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5', form: orderInfo }, function (err, httpResponse, body) {
+    //     console.log('finish');
+    //         // res.send(body);
+    //         res.send(body);
+    //     })
+
     admin.database().ref('donate-request/' + tradeNo).update(orderInfo, function (error) {
         if (error) {
             console.log(error);
@@ -88,6 +124,7 @@ app.post('/finishPay', function(req,res){
 
 app.post('/finishCreatOrder', function (req, res) {
     console.log("catch order");
+    res.send('success');
     admin.database().ref('donate/' + req.body.MerchantTradeNo).update(req.body,function(error){
         if(error)
             console.log(error);
@@ -114,7 +151,7 @@ function CheckMacValue(orderInfo){
 
 function CreateNewOrder(orderInfo){
     var http = new XMLHttpRequest();
-    var url = 'https://payment-stage.ecpay.tw/Cashier/AioCheckOut/V5';
+    var url = 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5';
     http.open('POST', url, true);
     http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     var params = '';
