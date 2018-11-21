a1f88321caad0eb9a16c76a9f23a56e92c009d8b
Chen <sddd1234567@gmail.com>
2018-11-22 02:32:12 +0800

    新增atm捐款

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+const jsdom = require("jsdom");
 var request = require('request');
 var crypto = require('crypto');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
@@ -45,7 +46,7 @@ app.post('/', function (req, res) {
     res.redirect('/')
 })
 
-app.get('/donate', function (req, res) {
+app.get('/donatecvs', function (req, res) {
     
     // orderInfo.CheckMacValue = CheckMacValue(orderInfo);
     // console.log(JSON.stringify(orderInfo));
@@ -53,31 +54,21 @@ app.get('/donate', function (req, res) {
     // res.send(crypto.createHash('sha256').update('hashkey%3d5294y06jbispm5x9%26choosepayment%3dcvs%26choosesubpayment%3dcvs%26clientbackurl%3dhttps%3a%2f%2fdevelopers.opay.tw%2faiomock%2fmerchantclientbackurl%26encrypttype%3d1%26itemname%3d%e4%b8%ad%e9%80%94%e4%b9%8b%e5%ae%b6%e6%8d%90%e6%ac%be%26merchantid%3d2000132%26merchanttradedate%3d2018%2f11%2f2+21%3a15%3a5%26merchanttradeno%3dtfirslpmt0rba03hirab%26paymenttype%3daio%26returnurl%3dhttp%3a%2f%2f140.127.220.111%2ffinishpay%26storeid%3d%26totalamount%3d50%26tradedesc%3d%e6%8d%90%e6%ac%be%e7%b5%a6%e4%b8%ad%e9%80%94%e4%b9%8b%e5%ae%b6%26hashiv%3dv77hokgq4kwxnnis').digest());
     // CreateNewOrder(orderInfo);
     
-    // request.post({ url: 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5', form: orderInfo }, function (err, httpResponse, body) {
-    //     console.log('finish');
-    //         // res.send(body);
-    //         res.send(body);
-    //     })
+    var targetFundraisingKey = req.query.targetFundraisingKey;
+    var targetFundraisingName = req.query.targetFundraisingName;
+    var totalAmount = req.query.totalAmount;
+    var sponsor = req.query.sponsor;
+    var cvs_type = req.query.cvs_type;
+
     var tradeNo = CreateTradeNo();
     var orderDate = GetDateStr();
-    var orderInfo = {
-        ChooseSubPayment: "IBON",
-        ClientBackURL: '/',
-        ItemName: "中途之家捐款",
-        MerchantTradeDate: orderDate,
-        MerchantTradeNo: tradeNo,
-        PaymentType: "aio",
-        ReturnURL: "http://140.127.220.111/finishPay",
-        TotalAmount: 50,
-        TradeDesc: "捐款給中途之家"
-    }
     let base_param = {
-        ChooseSubPayment: "IBON",
+        ChooseSubPayment: cvs_type,
         MerchantTradeNo: tradeNo, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
         MerchantTradeDate: orderDate, //ex: 2017/02/13 15:45:30
-        TotalAmount: '50',
-        TradeDesc: '捐款給中途之家',
-        ItemName: '中途之家捐款',
+        TotalAmount: totalAmount,
+        TradeDesc: targetFundraisingName,
+        ItemName: targetFundraisingName,
         ReturnURL: 'http://140.127.220.111/finishPay',
         // ChooseSubPayment: '',
         // OrderResultURL: 'http://192.168.0.1/payment_result',
@@ -86,13 +77,13 @@ app.get('/donate', function (req, res) {
         // ItemURL: 'http://item.test.tw',
         // Remark: '交易備註',
         // StoreID: '',
-        // CustomField1: '',
-        // CustomField2: '',
+        CustomField1: targetFundraisingKey,
+        CustomField2: sponsor,
         // CustomField3: '',
         // CustomField4: ''
     };
     let cvs_params = {
-        StoreExpireDate: '7',
+        StoreExpireDate: '60',
         Desc_1: '超商螢幕描述A',
         Desc_2: '超商螢幕描述B',
         Desc_3: '超商螢幕描述C',
@@ -103,20 +94,84 @@ app.get('/donate', function (req, res) {
     let create = new ecpay_payment();
     let htm = create.payment_client.aio_check_out_cvs(cvs_info = cvs_params, parameters = base_param, invoice = inv_params, client_redirect_url = client_redirect);
     res.send(htm);
-    console.log(htm);
+    // console.log(htm);
+    // admin.database().ref('donate-request/' + tradeNo).update(orderInfo, function (error) {
+    //     if (error) {
+    //         console.log(error);
+    //     }
+    //     else {
+    //         admin.database().ref('donate/' + tradeNo).on('value', function (snapshot) {
+    //             if(snapshot.val() != null)
+    //                 res.send(tradeNo);
+    //             // res.send(tradeNo);
+    //         });
+    //     }
+    // });
+})
 
-    admin.database().ref('donate-request/' + tradeNo).update(orderInfo, function (error) {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            admin.database().ref('donate/' + tradeNo).on('value', function (snapshot) {
-                // if(snapshot.val() != null)
-                //     res.send(tradeNo);
-                // res.send(tradeNo);
-            });
-        }
-    });
+app.get('/donateatm', function(req,res){
+    var targetFundraisingKey = req.query.targetFundraisingKey;
+    var targetFundraisingName = req.query.targetFundraisingName;
+    var totalAmount = req.query.totalAmount;
+    var sponsor = req.query.sponsor;
+
+    var tradeNo = CreateTradeNo();
+    var orderDate = GetDateStr();
+    let base_param = {
+        MerchantTradeNo: tradeNo, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
+        MerchantTradeDate: orderDate, //ex: 2017/02/13 15:45:30
+        TotalAmount: totalAmount,
+        TradeDesc: targetFundraisingName,
+        ItemName: targetFundraisingName,
+        ReturnURL: 'http://140.127.220.111/finishPay',
+        EncryptType: '1',
+        ChooseSubPayment: 'CHINATRUST',
+        // OrderResultURL: 'http://192.168.0.1/payment_result',
+        // NeedExtraPaidInfo: '1',
+        // ClientBackURL: 'https://www.google.com',
+        // ItemURL: 'http://item.test.tw',
+        // Remark: '交易備註',
+        // StoreID: '',
+        CustomField1: targetFundraisingKey,
+        CustomField2: sponsor,
+        // CustomField3: '',
+        // CustomField4: ''
+    };
+
+    // 若要測試開立電子發票，請將inv_params內的"所有"參數取消註解 //
+    let inv_params = {
+        // RelateNumber: 'PLEASE MODIFY',  //請帶30碼uid ex: SJDFJGH24FJIL97G73653XM0VOMS4K
+        // CustomerID: 'MEM_0000001',  //會員編號
+        // CustomerIdentifier: '',   //統一編號
+        // CustomerName: '測試買家',
+        // CustomerAddr: '測試用地址',
+        // CustomerPhone: '0123456789',
+        // CustomerEmail: 'johndoe@test.com',
+        // ClearanceMark: '2',
+        // TaxType: '1',
+        // CarruerType: '',
+        // CarruerNum: '',
+        // Donation: '2',
+        // LoveCode: '',
+        // Print: '1',
+        // InvoiceItemName: '測試商品1|測試商品2',
+        // InvoiceItemCount: '2|3',
+        // InvoiceItemWord: '個|包',
+        // InvoiceItemPrice: '35|10',
+        // InvoiceItemTaxType: '1|1',
+        // InvoiceRemark: '測試商品1的說明|測試商品2的說明',
+        // DelayDay: '0',
+        // InvType: '07'
+    };
+
+    let pay_info_url = 'http://140.127.220.111/finishCreateOrder';
+    let exp = '60';
+    let cli_redir_url = '';
+
+    let create = new ecpay_payment();
+    let htm = create.payment_client.aio_check_out_atm(parameters = base_param, url_return_payinfo = pay_info_url, exp_period = exp, client_redirect = cli_redir_url, invoice = inv_params);
+    console.log(htm);
+    res.send(htm);
 })
 
 app.post('/finishPay', function(req,res){
@@ -126,7 +181,9 @@ app.post('/finishPay', function(req,res){
 app.post('/finishCreateOrder', function (req, res) {
     console.log(JSON.stringify(req.body));
     res.send(JSON.stringify(req.body));
-    admin.database().ref('donate/' + req.body.MerchantTradeNo).update(req.body, function (error) {
+    var obj = req.body;
+    obj['finishPay'] = false;
+    admin.database().ref('donate/' + req.body.MerchantTradeNo).update(obj, function (error) {
         if (error)
             console.log(error);
         else
