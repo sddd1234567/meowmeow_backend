e3483a89fbc1721c24dda4b3b7bc7a5aac8242b5
Chen <sddd1234567@gmail.com>
2018-11-25 16:32:32 +0800

    募款正式環境

--- app.js
+++ app.js
@@ -281,7 +281,7 @@ function CreateNewOrder(orderInfo){
 
 function GetDateStr(){
     var date = new Date();
-    var str = date.getFullYear() + "/" + ('0' + (date.getMonth() + 1)).slice(-2) + "/" + ('0' + date.getDay()).slice(-2) + " " + ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2) + ":" + ('0' + date.getSeconds()).slice(-2);
+    var str = date.getFullYear() + "/" + ('0' + (date.getMonth() + 1)).slice(-2) + "/" + ('0' + date.getDate()).slice(-2) + " " + ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2) + ":" + ('0' + date.getSeconds()).slice(-2);
     return str;
 }
 
